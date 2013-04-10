28b69dfc0bfbbc65df54a360da36b6716ef0127e
David Coss <David.Coss@stjude.org>
2013-04-10 11:55:03 -0500

    Fixed typo.

--- ple/settings.py
+++ ple/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = local_settings.static_root
+STATIC_ROOT = local.static_root
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    local_settings.staticfiles_source,
+    local.staticfiles_source,
 )
 
 # List of finder classes that know how to find static files in
@@ -85,7 +85,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = local_settings.secret_key
+SECRET_KEY = local.secret_key
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -131,7 +131,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = local_settings.from_email
+DEFAULT_FROM_EMAIL = local.from_email
 
 ACCOUNT_ACTIVATION_DAYS=7
 
