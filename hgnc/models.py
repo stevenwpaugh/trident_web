9b8ee3f23ad96397892b52605ee35985ae98e145
stevepaugh <stevepaugh@gmail.com>
2013-04-14 13:40:59 -0500

    Added model to hgnc

--- hgnc/models.py
+++ hgnc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class hgncsymbols(models.Model):
+    hgnc_id = models.PositiveIntegerField()
+    approved_symbol = models.CharField()
+    approved_name = models.CharField()
+    status = models.CharField()
+    previous_symbols = models.CharField()
+    previous_names = models.CharField()
+    synonyms = models.CharField()
+    chromosome = models.CharField()
+    accession_numbers = models.CharField()
+    refseq_ids = models.CharField()
