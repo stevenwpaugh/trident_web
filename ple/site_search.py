1e5751de3a29ed97fac0b3766f551af788f490da
unknown <spaugh@A41047.stjude.sjcrh.local>
2013-04-14 21:02:59 -0500

    fixed missing paran in site_search.py

--- ple/site_search.py
+++ ple/site_search.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 import ple_interface
 from tridentdb.models import *
 from affyannodb.models import *
+from hgnc.models import *
 from django.contrib.auth.decorators import login_required
 
 class SearchForm(forms.Form):
@@ -15,13 +16,13 @@ def site_search(request):
         if form.is_valid():
             userquery = request.POST['search_text']
             #userquery = form.search_interface.run_search(form.cleaned_data)
-            search_result_list = MicroRNA.objects.filter(mirbase_name__icontains=userquery)
-	    affy_result_list = expression.objects.filter(gene_symbol__icontains=userquery)
-            genes = Genes.objects.filter(name__icontains=userquery)
-            gene_dict = {}
-            for gene in genes:
-                gene_dict[gene.name] = gene
-            return render_to_response('search/search_output.html',{'msg': search_result_list, 'msg2': affy_result_list, 'genes': gene_dict})
+            #search_result_list = MicroRNA.objects.filter(mirbase_name__icontains=userquery)
+	    #affy_result_list = expression.objects.filter(gene_symbol__icontains=userquery)
+            genes = hgncsymbols.objects.filter(approved_symbol__icontains=userquery)
+            #gene_dict = {}
+            #for gene in genes:
+            #    gene_dict[gene.name] = gene
+            return render_to_response({'genes': gene_dict})
     else:
         form = SearchForm()
         
