948e9798c214dcb08d53660e711677d797571e01
David Coss (cluster) <David.Coss@stjude.org>
2013-05-24 09:36:11 -0500

    Changed the search feature to search microrna.

--- ple/site_search.py
+++ ple/site_search.py
@@ -15,15 +15,10 @@ def site_search(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            userquery = request.POST['search_text']
-            #userquery = form.search_interface.run_search(form.cleaned_data)
-            #search_result_list = MicroRNA.objects.filter(mirbase_name__icontains=userquery)
-	    #affy_result_list = expression.objects.filter(gene_symbol__icontains=userquery)
+            userquery = form.cleaned_data['search_text']
+            mirnas = MicroRNA.objects.filter(mirbase_name__icontains=userquery)
             genes = hgncsymbols.objects.filter(approved_symbol__icontains=userquery)
-            #gene_dict = {}
-            #for gene in genes:
-            #    gene_dict[gene.approved_symbol] = gene
-            return render_to_response('search/search_output.html', {'genes': genes})
+            return render_to_response('search/search_output.html', {'genes': genes, "mirnas": mirnas})
     else:
         form = SearchForm()
         
