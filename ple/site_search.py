482bfb7354f46b06eb0d2f1ded32945fa858366a
David Coss (cluster) <David.Coss@stjude.org>
2013-05-24 10:06:29 -0500

    Made the search feature RESTful.

--- ple/site_search.py
+++ ple/site_search.py
@@ -9,17 +9,26 @@ from django.contrib.auth.decorators import login_required
 
 class SearchForm(forms.Form):
     search_text = forms.CharField()
-    
+
+def perform_search(query):
+    mirnas = MicroRNA.objects.filter(mirbase_name__icontains=query)
+    genes = hgncsymbols.objects.filter(approved_symbol__icontains=query)
+    return render_to_response('search/search_output.html', {'genes': genes, "mirnas": mirnas})
+
 #@login_required    
 def site_search(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             userquery = form.cleaned_data['search_text']
-            mirnas = MicroRNA.objects.filter(mirbase_name__icontains=userquery)
-            genes = hgncsymbols.objects.filter(approved_symbol__icontains=userquery)
-            return render_to_response('search/search_output.html', {'genes': genes, "mirnas": mirnas})
+            return perform_search(userquery)
     else:
         form = SearchForm()
         
     return render_to_response('search/search_form.html', {'form': form}, context_instance=RequestContext(request))
+
+#@login_required    
+def site_search_restapi(request, query):
+    if not query:
+        return render_to_response('search/search_form.html', {'form': SearchForm()}, context_instance=RequestContext(request))
+    return perform_search(query)
