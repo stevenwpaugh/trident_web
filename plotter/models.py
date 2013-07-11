a4f2de2b3dac1b326f58c335d09283401ea2d31c
Steven Paugh <steven.paugh@stjude.org>
2013-07-11 11:29:12 -0500

    Removed unneeded plotter directory app

--- plotter/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class School(models.Model):
-    city = models.CharField("City", max_length=100)
-    zip = models.CharField("Zipcode", max_length=10)
-    def __unicode__(self):
-        return self.name + ' (' + self.city + ', ' + self.state + ')'
