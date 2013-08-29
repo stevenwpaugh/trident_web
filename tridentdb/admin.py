1b1599420ebc96c4791f0fdc56dd8d118ee66d90
David Coss <David.Coss@stjude.org>
2013-08-29 14:21:14 -0500

    Added columns for microrna admin page.

--- tridentdb/admin.py
+++ tridentdb/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 admin.site.register(TaxonomyGroup)
 admin.site.register(Genome)
-admin.site.register(MicroRNA)
+admin.site.register(MicroRNAGeneAssociation)
 admin.site.register(MatchType)
 class ResultsAdmin(admin.ModelAdmin):
 	#search_fields = ['hit_genomic_start']
@@ -12,6 +12,6 @@ class ResultsAdmin(admin.ModelAdmin):
 admin.site.register(Results,ResultsAdmin)
 admin.site.register(AffymetrixID)
 
-class GenesAdmin(admin.ModelAdmin):
-    list_display = ("name", "genome")
-admin.site.register(Genes, GenesAdmin)
+class MicroRNAAdmin(admin.ModelAdmin):
+	list_display = ("mirbase_name", "chromosome", "genome")
+admin.site.register(MicroRNA, MicroRNAAdmin)
